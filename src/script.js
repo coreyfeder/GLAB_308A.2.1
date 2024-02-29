8657a48a22a77c8ad3558956b49d63dd8d4211bf
Corey Feder <corey.feder@gmail.com>
2024-02-29 11:09:01 -0500

    roll, Robin, roll

--- src/script.js
+++ src/script.js
@@ -16,4 +16,12 @@ const adventurer = {
             inventory: ["small hat", "sunglasses"],
         },
     },
+    roll (mod = 0) {
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a ${result}.`)
+    },
 }
+
+adventurer.roll();
+adventurer.roll();
+adventurer.roll();
