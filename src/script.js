fb18f791534c2ea94c1851454a30e46e57ee6935
Corey Feder <corey.feder@gmail.com>
2024-02-29 11:20:26 -0500

    Robin 2.0

--- src/script.js
+++ src/script.js
@@ -37,6 +37,10 @@ const adventurer = {
     },
 }
 
-adventurer.roll();
-adventurer.roll();
-adventurer.roll();
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
